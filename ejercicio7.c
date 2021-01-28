bf8177b2dbd3124a075aeffcf175058c69c4b0ad
Allan Alonzo <66280187+plasticoaa@users.noreply.github.com>
2021-01-28 08:59:58 -0600

    Lab ipc day 2

---
+++ ejercicio7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+     float *ptr_float; 
+     printf("valor apuntado  %f", *ptr_float);
+     *ptr_float =10.5;
+     
+return 0;
+}
---
+++ ejercicio9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a = 10, b = 15;
+    
+    int *const ptr = &a;
+    *ptr = 3;
+    ptr = &b;
+    
+    
+    return 0;
+}
